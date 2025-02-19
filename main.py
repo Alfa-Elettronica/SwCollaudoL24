e0d976fa9f66b7b79cefed8a65ef47dd5f42519b
Donald Fontanelli <donald.fontanelli@alfaelettronica.com>
2025-02-19 13:17:51 +0100

    load 3 cert

--- main.py
+++ main.py
@@ -67,6 +67,12 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
         self.btn_wr_cert1.clicked.connect(self.__btn_wr_cert1_click__)
         self.btn_tx_cert1.clicked.connect(self.__btn_tx_cert1_click__)
         self.btn_tx_cert1_modem.clicked.connect(self.__btn_tx_cert1_to_modem_click__)
+        self.btn_wr_cert2.clicked.connect(self.__btn_wr_cert2_click__)
+        self.btn_tx_cert2.clicked.connect(self.__btn_tx_cert2_click__)
+        self.btn_tx_cert2_modem.clicked.connect(self.__btn_tx_cert2_to_modem_click__)
+        self.btn_wr_cert3.clicked.connect(self.__btn_wr_cert3_click__)
+        self.btn_tx_cert3.clicked.connect(self.__btn_tx_cert3_click__)
+        self.btn_tx_cert3_modem.clicked.connect(self.__btn_tx_cert3_to_modem_click__)
         self.dut_status_signal.connect(self.__update_dut_status_ui__)
 
     def __write_idt__(self):
@@ -234,8 +240,18 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
         return rcv
     
 
-    def __wr_cert1__(self):
-        client_cert = "-----BEGIN CERTIFICATE-----\r\nMIIDWTCCAkGgAwIBAgIUe5jM1v1Ps8cI4QMftNePg2eiLuAwDQYJKoZIhvcNAQEL\r\nBQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g\r\nSW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTIzMDcwNzA4MjI1\r\nMVoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0\r\nZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMA3VKfjT6fDhvVOZWJ0\r\n1pjPT9oL4bKaVUs1tv4jvDD4J5DixEVaRei/Rctt7yvOcxI0eJ7AiA9hn8qZGoUX\r\n5Sk32Adh8ycNmFDU6WY+8W4FTkMCTgdT+4cj1clImtsNLkOcNWIFN1+aofMBRZ9W\r\nb+hqOGj8BHbHMFNIaYrCSr68tXPdDQ7YeCODeOPQ/G4u7OUrt8ChXyDpki2ry4Em\r\nBPWoNjbseOSiS4NYPKumq6jhKSs5LUe20ePxy29GMJkto+x1BskZOtVFU81c9xj6\r\nmIRL0r6VTEIoTJyZFyFXJv8Ow4vdzeSPp/KzTcmAYDLBdsfz8xs3LprhkIJu7Ur5\r\n7h8CAwEAAaNgMF4wHwYDVR0jBBgwFoAUIyuhjCHkrPCUa7woub0GF/HHvQAwHQYD\r\nVR0OBBYEFDq4Ob+pDT8FBHGpJcpUYC1oEN4jMAwGA1UdEwEB/wQCMAAwDgYDVR0P\r\nAQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQA5AKi0o0X1w6D2NgCpMDi/C0tZ\r\nxT+gGqpIS7DGyo/1iLFuN0vcQ91Z5Uh+OfAuc1l9AhPRRINhcIk3XAXChOVlcTGE\r\na0XHCjV7O63E4MWN/jNEqf0F1rI61mdq9e3CFrlzDBNiIdj+l8VuptoNI38jdz98\r\nvNhXlC7qMPnWcwl9w8Ygy84ab5rTS8cwPgAnrLDkualZyGL1E0KmjowkIJFNgdJB\r\nDNXWvzR1Uj73nmY+Hgkt9jl7zsTiFLqhWQgKU2OfjPXJIQriww1veiSxkyBitb0Q\r\nJyot72zoowS37k0HF8x/GSS75U79sNEFgpFRLb0wQO+POJ4nwvNG5USbd54A\r\n-----END CERTIFICATE-----"
+    def __wr_cert__(self, cert_type):
+        cli_cert = "-----BEGIN CERTIFICATE-----\r\nMIIDWTCCAkGgAwIBAgIUe5jM1v1Ps8cI4QMftNePg2eiLuAwDQYJKoZIhvcNAQEL\r\nBQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g\r\nSW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTIzMDcwNzA4MjI1\r\nMVoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0\r\nZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMA3VKfjT6fDhvVOZWJ0\r\n1pjPT9oL4bKaVUs1tv4jvDD4J5DixEVaRei/Rctt7yvOcxI0eJ7AiA9hn8qZGoUX\r\n5Sk32Adh8ycNmFDU6WY+8W4FTkMCTgdT+4cj1clImtsNLkOcNWIFN1+aofMBRZ9W\r\nb+hqOGj8BHbHMFNIaYrCSr68tXPdDQ7YeCODeOPQ/G4u7OUrt8ChXyDpki2ry4Em\r\nBPWoNjbseOSiS4NYPKumq6jhKSs5LUe20ePxy29GMJkto+x1BskZOtVFU81c9xj6\r\nmIRL0r6VTEIoTJyZFyFXJv8Ow4vdzeSPp/KzTcmAYDLBdsfz8xs3LprhkIJu7Ur5\r\n7h8CAwEAAaNgMF4wHwYDVR0jBBgwFoAUIyuhjCHkrPCUa7woub0GF/HHvQAwHQYD\r\nVR0OBBYEFDq4Ob+pDT8FBHGpJcpUYC1oEN4jMAwGA1UdEwEB/wQCMAAwDgYDVR0P\r\nAQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQA5AKi0o0X1w6D2NgCpMDi/C0tZ\r\nxT+gGqpIS7DGyo/1iLFuN0vcQ91Z5Uh+OfAuc1l9AhPRRINhcIk3XAXChOVlcTGE\r\na0XHCjV7O63E4MWN/jNEqf0F1rI61mdq9e3CFrlzDBNiIdj+l8VuptoNI38jdz98\r\nvNhXlC7qMPnWcwl9w8Ygy84ab5rTS8cwPgAnrLDkualZyGL1E0KmjowkIJFNgdJB\r\nDNXWvzR1Uj73nmY+Hgkt9jl7zsTiFLqhWQgKU2OfjPXJIQriww1veiSxkyBitb0Q\r\nJyot72zoowS37k0HF8x/GSS75U79sNEFgpFRLb0wQO+POJ4nwvNG5USbd54A\r\n-----END CERTIFICATE-----"
+        # cli_cert = "-----BEGIN CERTIFICATE-----\r\nMIIDaaaCAkGgAwIBAgIUe5jM1v1Ps8cI4QMftNePg2eiLuAwDQYJKoZIhvcNAQEL\r\nBQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g\r\nSW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTIzMDcwNzA4MjI1\r\nMVoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0\r\nZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMA3VKfjT6fDhvVOZWJ0\r\n1pjPT9oL4bKaVUs1tv4jvDD4J5DixEVaRei/Rctt7yvOcxI0eJ7AiA9hn8qZGoUX\r\n5Sk32Adh8ycNmFDU6WY+8W4FTkMCTgdT+4cj1clImtsNLkOcNWIFN1+aofMBRZ9W\r\nb+hqOGj8BHbHMFNIaYrCSr68tXPdDQ7YeCODeOPQ/G4u7OUrt8ChXyDpki2ry4Em\r\nBPWoNjbseOSiS4NYPKumq6jhKSs5LUe20ePxy29GMJkto+x1BskZOtVFU81c9xj6\r\nmIRL0r6VTEIoTJyZFyFXJv8Ow4vdzeSPp/KzTcmAYDLBdsfz8xs3LprhkIJu7Ur5\r\n7h8CAwEAAaNgMF4wHwYDVR0jBBgwFoAUIyuhjCHkrPCUa7woub0GF/HHvQAwHQYD\r\nVR0OBBYEFDq4Ob+pDT8FBHGpJcpUYC1oEN4jMAwGA1UdEwEB/wQCMAAwDgYDVR0P\r\nAQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQA5AKi0o0X1w6D2NgCpMDi/C0tZ\r\nxT+gGqpIS7DGyo/1iLFuN0vcQ91Z5Uh+OfAuc1l9AhPRRINhcIk3XAXChOVlcTGE\r\na0XHCjV7O63E4MWN/jNEqf0F1rI61mdq9e3CFrlzDBNiIdj+l8VuptoNI38jdz98\r\nvNhXlC7qMPnWcwl9w8Ygy84ab5rTS8cwPgAnrLDkualZyGL1E0KmjowkIJFNgdJB\r\nDNXWvzR1Uj73nmY+Hgkt9jl7zsTiFLqhWQgKU2OfjPXJIQriww1veiSxkyBitb0Q\r\nJyot72zoowS37k0HF8x/GSS75U79sNEFgpFRLb0wQO+POJ4nwvNG5USbd54A\r\n-----END CERTIFICATE-----"
+        cli_key = "-----BEGIN RSA PRIVATE KEY-----\r\nMIIEogIBAAKCAQEAwDdUp+NPp8OG9U5lYnTWmM9P2gvhsppVSzW2/iO8MPgnkOLE\r\nRVpF6L9Fy23vK85zEjR4nsCID2GfypkahRflKTfYB2HzJw2YUNTpZj7xbgVOQwJO\r\nB1P7hyPVyUia2w0uQ5w1YgU3X5qh8wFFn1Zv6Go4aPwEdscwU0hpisJKvry1c90N\r\nDth4I4N449D8bi7s5Su3wKFfIOmSLavLgSYE9ag2Nux45KJLg1g8q6arqOEpKzkt\r\nR7bR4/HLb0YwmS2j7HUGyRk61UVTzVz3GPqYhEvSvpVMQihMnJkXIVcm/w7Di93N\r\n5I+n8rNNyYBgMsF2x/PzGzcumuGQgm7tSvnuHwIDAQABAoIBAE0Uoy0kOagz/6XV\r\nh1ChPAFReVseUqbVvwiHBNgLKoeUrAEs/ro1Bj3cnjeC4Vt20axmQEyhNq68XmDX\r\nXswqleoei9ICFIj/qaoYh3RKH3UYSZcTkIjdw8sgsrWiGP9o3LmeJcYmA1uiXfld\r\n9DZ+aigQmIh5L60WGan8Kt7LJUAxJ5DA0admcJUUSTrofxEbKRf3Aqo2A8JUWzLM\r\nuX8DbULDnvrxx48RqrEKe7CQUCcy5+cfbgKLwBYnVsS5U0IcQX+tAnp7WA0sl9zU\r\nVL64+3+DFc6vA7SwtDUjGbM3AWKXY6QPRK2LwjkNY4kBWtsivxCGKbky9Dj7Wzjc\r\nozb/SdkCgYEA+TAE8OxhODCa4SnYjOtylsqtUyr7jy3pRVQKTbsduW8847GrVRuO\r\n5IcdOxMYDesnK0VyX2A1CD2F7FJZEzhNgMTQwYDngd8FKDfueKUTJ0xe4iUo1M1L\r\nzkHW6/V/36fBfbYRDcBID5S6LcfmnBJlzEs+uJHyYqC9MlF1rPNfzCsCgYEAxXiW\r\nWGO6kbVvZ0X/i/K77N8fP2RIuBIrvUceeT8hucy4ZQhNAk66aXZZpNfD/7q3TWSA\r\njUOb/Afk6ZV1fLpFtZIVxRPZNjIBMfM8AFtII0bhwRNpErCjF3GWjPJ11be1omD8\r\n83Lf4uV49+O0+8Vwyr8NAtMZFdpBKPSNZkvUh90CgYBD0CCYHAv9CaUsf4HSH8UI\r\nalGu33SkK19fIZbIPpLBQxdz84bn218QrAB1ciXKq+L18KlGcV0dR/jpLiPVii31\r\nTBpvfpACFNpHbqk0JeBHgo4Txv8Mom3tzJcbkaziBbovZtvPPTOfId9k1BDbClqv\r\ntQ51lio7UvkJ94cpsPWyDwKBgAy00q/DUwj3LMDvbx8ZMmBuhvs0P72gZbIbNmnE\r\n1y22b5MIsrPYTwRkOiZyP8lfwVW4htEQLaRM+bzSAipRbhTd3oq82Tg0hYEqTo0T\r\nUpP6hqI+1n7+YLAsfex52X00Afr91KjxllhqPZttyoJ81OIm4vZwkOeoEJNLESIo\r\n9Pb9AoGAaW0MHv8TEKWMfiEZihGYXicm7TtLfJQ49fS9TpnpjUdmLIBxIbV8gwnV\r\nR96Wpvca0wkBCWiOprYXkji31a/OhUyNxNOosnTwfvCv0fH0mM2xIatwu/piraHU\r\ns0VbzdaVQZwHQ1NjdKrFOP1AbZfT06DQw6XQrbQjIM2uqxw/KtA=\r\n-----END RSA PRIVATE KEY-----"
+        ca_cert = "-----BEGIN CERTIFICATE-----\r\nMIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\r\nADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\r\nb24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\r\nMAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\r\nb3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\r\nca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\r\n9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\r\nIFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\r\nVOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\r\n93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\r\njgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\r\nAYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\r\nA4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\r\nU5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\r\nN+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\r\no/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\r\n5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\r\nrqXRfboQnoZsG4q5WTP468SQvvG5\r\n-----END CERTIFICATE-----\r\n"
+        dw_cert = ""
+        if(cert_type == 1) :
+            dw_cert = cli_cert
+        if(cert_type == 2) :
+            dw_cert = cli_key
+        if(cert_type == 3) :
+            dw_cert = ca_cert
         ser_port_mame = ''
         if self.__host_plat == "Windows":
             ser_port_mame = self.comboPort.currentText()
@@ -254,22 +270,22 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
         ser.isOpen()
 
         idx_start = 0
-        bytes_to_transfer = len(client_cert)
+        bytes_to_transfer = len(dw_cert)
 
         tx_size = 512
         while(bytes_to_transfer > 0):
             tx_size = min(tx_size, bytes_to_transfer)
-            cert_str = client_cert[idx_start : idx_start + tx_size]
+            cert_str = dw_cert[idx_start : idx_start + tx_size]
             cert_b = bytearray()
             cert_b.extend(map(ord, cert_str))
-            cmd_str = b'{WRCERT\x00\x00;' + b'\x01' + idx_start.to_bytes(2, 'little') + tx_size.to_bytes(2, 'little')
+            cmd_str = b'{WRCERT\x00\x00;' + cert_type.to_bytes(1, 'little') + idx_start.to_bytes(2, 'little') + tx_size.to_bytes(2, 'little')
             cmd_str = cmd_str + cert_b
             buff_crc = self.__calcola_crc16__(len(cmd_str), cmd_str)
             cmd_str = cmd_str + buff_crc[0] + buff_crc[1] + b'}'
             ser.write(cmd_str)
             # let's wait one second before reading output (let's give device time to answer)
             time.sleep(0.3)
-            rcv = ser.read_all()            
+            rcv = ser.read_all()      
             idx_start = idx_start + tx_size
             bytes_to_transfer = bytes_to_transfer - tx_size
             if len(rcv) != 0:
@@ -458,13 +474,31 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
         self.__read_status__()
 
     def __btn_wr_cert1_click__(self):
-        self.__wr_cert1__()
+        self.__wr_cert__(1)
  
     def __btn_tx_cert1_click__(self):
-        self.__tx_cert1__()
+        self.__tx_cert__()
 
     def __btn_tx_cert1_to_modem_click__(self):
-        self.__tx_cert1_to_modem__()
+        self.__tx_cert_to_modem__()
+
+    def __btn_wr_cert2_click__(self):
+        self.__wr_cert__(2)
+ 
+    def __btn_tx_cert2_click__(self):
+        self.__tx_cert__()
+
+    def __btn_tx_cert2_to_modem_click__(self):
+        self.__tx_cert_to_modem__()
+
+    def __btn_wr_cert3_click__(self):
+        self.__wr_cert__(3)
+ 
+    def __btn_tx_cert3_click__(self):
+        self.__tx_cert__()
+
+    def __btn_tx_cert3_to_modem_click__(self):
+        self.__tx_cert_to_modem__()
 
     def __calcola_crc16__(self, b_len, buf):
         cy = int(0)
@@ -541,7 +575,7 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
         else:
             self.start_fase_programmazione()
 
-    def __tx_cert1__(self):
+    def __tx_cert__(self):
         '''Avvio trasferimento certificato a uCm
         '''
         ser_port_mame = ''
@@ -581,7 +615,7 @@ class MainWindowCollaudo(QtWidgets.QMainWindow, Ui_MainWindow):     # pylint: di
             print('No response')
         return rcv
 
-    def __tx_cert1_to_modem__(self):
+    def __tx_cert_to_modem__(self):
         ser_port_name = ''
         if self.__host_plat == "Windows":
             ser_port_name = self.comboPort.currentText()
